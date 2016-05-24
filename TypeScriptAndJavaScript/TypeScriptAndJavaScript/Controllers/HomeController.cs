30abdc0abdb4650faf7f5ed2357f0a40f9da1b5c
David Opdendries <david@zalo.se>
2016-05-24 11:15:46 +0200

    Assignment 6

---
+++ TypeScriptAndJavaScript/TypeScriptAndJavaScript/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TypeScriptAndJavaScript.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Assignment6()
+        {
+            return View();
+        }
+    }
+}
---
+++ TypeScriptAndJavaScript/TypeScriptAndJavaScript/Scripts/Assignment6.ts
@@ -0,0 +1,46 @@
+﻿
+(function () {
+
+
+
+    $(document).ready(
+        function () {
+            $('#colorChangerButton').click(ChangeColorOfPage);
+            //$('#colors').change(ShowPreview);
+            var options = $('#colors option');
+            //console.log(options);
+
+            // foreach (var option in options)
+            $.each(options, function (index, option) {
+                var color = option.value;
+                //javascript
+                option.setAttribute("style", "color:grey;background-color:" + color);
+                // jquery
+               // $(value).attr("style", "background-color:" + color);
+
+            });
+
+        }
+    );
+
+    function ShowPreview() {
+        var color = $('#colors').val();
+        $('#colors').attr("style", "background-color:" + color);
+    }
+
+    function ChangeColorOfPage() {
+        var color = $('#colors').val();
+        console.log(color);
+
+        if (color == "none") {
+            document.body.style.backgroundColor = null;
+        }
+        else {
+            document.body.style.backgroundColor = color;
+            //$(document.body).attr("style", "background-color:" + color);
+            //$('body').attr("style", "background-color:" + color);
+           
+        }
+    }
+
+})();
