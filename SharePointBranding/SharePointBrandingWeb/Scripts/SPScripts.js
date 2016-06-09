a81a25bbe456559939498a9839a6a0e1a9518d34
David Opdendries <david@zalo.se>
2016-06-09 10:06:30 +0200

    Stutus and notifications

--- SharePointBranding/SharePointBrandingWeb/Scripts/SPScripts.js
+++ SharePointBranding/SharePointBrandingWeb/Scripts/SPScripts.js
@@ -1,3 +1,5 @@
+var statusId = '';
+var notifyId = '';
 var spcustomization;
 (function (spcustomization) {
     var FixBranding = (function () {
@@ -13,6 +15,40 @@ var spcustomization;
         return FixBranding;
     }());
     spcustomization.FixBranding = FixBranding;
+    var Notifs = (function () {
+        function Notifs() {
+        }
+        Notifs.AddNotification = function () {
+            //notifyId = SP.UI.Notify.addNotification("Hello World!", true);
+            notifyId = SP.UI.Notify.addNotification("Hello World!", false);
+        };
+        Notifs.RemoveNotification = function () {
+            SP.UI.Notify.removeNotification(notifyId);
+            notifyId = '';
+        };
+        Notifs.AddStatus = function () {
+            statusId = SP.UI.Status.addStatus("Status good! <a href='#'>Good stuff here</a>");
+            // red error message
+            // yellow information
+            // green Good message
+            SP.UI.Status.setStatusPriColor(statusId, 'green');
+        };
+        Notifs.RemoveLastStatus = function () {
+            SP.UI.Status.removeStatus(statusId);
+            statusId = '';
+        };
+        Notifs.RemoveAllStatus = function () {
+            SP.UI.Status.removeAllStatus(true);
+        };
+        return Notifs;
+    }());
+    spcustomization.Notifs = Notifs;
 })(spcustomization || (spcustomization = {}));
 _spBodyOnLoadFunctionNames.push("spcustomization.FixBranding.Init");
-//# sourceMappingURL=SPScripts.js.map
+// html used for notifications
+//<input id="Button1" type= "button" value= "Add Notification" onclick= "spcustomization.Notifs.AddNotification()" />
+//    <input id="Button2" type= "button" value= "Remove Notification" onclick= "spcustomization.Notifs.RemoveNotification()" />
+//        <p></p>
+//        < input id= "Button3" type= "button" value= "Add Status" onclick= "spcustomization.Notifs.AddStatus()" />
+//            <input id="Button4" type= "button" value= "Remove Last Status" onclick= "spcustomization.Notifs.RemoveLastStatus()" />
+//                <input id="Button5" type= "button" value= "Remove All Status" onclick= "spcustomization.Notifs.RemoveAllStatus()" /> 
--- SharePointBranding/SharePointBrandingWeb/Scripts/SPScripts.ts
+++ SharePointBranding/SharePointBrandingWeb/Scripts/SPScripts.ts
@@ -1,4 +1,7 @@
-﻿namespace spcustomization {
+﻿var statusId = '';
+var notifyId = '';
+
+namespace spcustomization {
     export class FixBranding
     {
         static Init() {
@@ -13,9 +16,47 @@
             element.insertBefore(newElement, element.firstChild);
         }
     }
+
+    export class Notifs {
+
+       static AddNotification() {
+            //notifyId = SP.UI.Notify.addNotification("Hello World!", true);
+            notifyId = SP.UI.Notify.addNotification("Hello World!", false);
+
+        }
+
+       static RemoveNotification() {
+            SP.UI.Notify.removeNotification(notifyId);
+            notifyId = '';
+        }
+
+       static AddStatus() {
+           statusId = SP.UI.Status.addStatus("Status good! <a href='#'>Good stuff here</a>");
+           // red error message
+           // yellow information
+           // green Good message
+            SP.UI.Status.setStatusPriColor(statusId, 'green');
+        }
+
+       static RemoveLastStatus() {
+            SP.UI.Status.removeStatus(statusId);
+            statusId = '';
+        }
+
+       static RemoveAllStatus() {
+            SP.UI.Status.removeAllStatus(true);
+        }
+    }
 }
 
 _spBodyOnLoadFunctionNames.push("spcustomization.FixBranding.Init");
 
 
 
+// html used for notifications
+//<input id="Button1" type= "button" value= "Add Notification" onclick= "spcustomization.Notifs.AddNotification()" />
+//    <input id="Button2" type= "button" value= "Remove Notification" onclick= "spcustomization.Notifs.RemoveNotification()" />
+//        <p></p>
+//        < input id= "Button3" type= "button" value= "Add Status" onclick= "spcustomization.Notifs.AddStatus()" />
+//            <input id="Button4" type= "button" value= "Remove Last Status" onclick= "spcustomization.Notifs.RemoveLastStatus()" />
+//                <input id="Button5" type= "button" value= "Remove All Status" onclick= "spcustomization.Notifs.RemoveAllStatus()" />
