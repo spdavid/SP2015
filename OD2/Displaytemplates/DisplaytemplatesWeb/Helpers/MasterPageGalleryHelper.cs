ae1f5785bd67df577d1026834e166e96bf4e96db
David Opdendries <david@zalo.se>
2016-08-30 11:01:02 +0200

    asdfadf

---
+++ OD2/Displaytemplates/DisplaytemplatesWeb/Helpers/MasterPageGalleryHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace DisplaytemplatesWeb.Helpers
+{
+    public class MasterPageGalleryHelper
+    {
+        public static void UploadDisplaytemplate(ClientContext ctx)
+        {
+
+            List list = ctx.Web.GetListByUrl("/_catalogs/masterpage");
+            Folder rootFolder = list.RootFolder;
+            Folder dtFolder = rootFolder.EnsureFolder("Display Templates");
+            Folder cwpFolder = dtFolder.EnsureFolder("Content Web Parts");
+
+            string path = HttpContext.Current.Server.MapPath("./SPContent/Item_FavoriteFood.html");
+
+            cwpFolder.UploadFile("Item_FavoriteFood.html", path, true);
+
+
+
+
+        }
+
+    }
+}
